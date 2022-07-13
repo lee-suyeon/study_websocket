974350cda6b5f74c55895c7cc1ba90a8a1eb113b
leesuyeon <sroovy@naver.com>
2022-07-13 10:41:57 +0900

    1.2 WebSockets in NodeJs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,4 +11,10 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen); 
+
+// http 서버에 접근
+const server = http.createServer(app);
+// http서버위에 websocket 서버 생성
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
