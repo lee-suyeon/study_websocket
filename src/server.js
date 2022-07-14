e7671973ff129f41822c01eee9b24d8a311f3dd8
leesuyeon <sroovy@naver.com>
2022-07-14 11:15:15 +0900

    1.3 WebSocket Events

--- src/server.js
+++ src/server.js
@@ -17,4 +17,9 @@ const server = http.createServer(app);
 // http서버위에 websocket 서버 생성
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  console.log(socket)
+;}
+wss.on("connection", handleConnection)
+
 server.listen(3000, handleListen);
