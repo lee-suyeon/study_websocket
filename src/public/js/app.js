73a0d8292aaba83bac221fd3c5a0f8729b7a1700
leesuyeon <sroovy@naver.com>
2022-07-15 11:11:43 +0900

    1.6 Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`); // 현재 위치 지정
 
 socket.addEventListener("open", () => {
@@ -10,8 +12,13 @@ socket.addEventListener("message", (message) => {
 
 socket.addEventListener("close", () => {
   console.log("Connected from Server ❌")
-})
+});
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000)
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit)
