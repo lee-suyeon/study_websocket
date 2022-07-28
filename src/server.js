d5a2aad66981429d5676862f820a0cd20b0ff057
leesuyeon <sroovy@naver.com>
2022-07-28 11:48:28 +0900

    Nicknames

--- src/server.js
+++ src/server.js
@@ -15,21 +15,21 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-  socket.onAny(event => {
-    console.log("socketEvent", event)
-  })
+  socket["nickname"] = "???";
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    console.log(socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach(room => socket.to(room).emit("bye"));
+    socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   })
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 // const wss = new WebSocket.Server({ server });
